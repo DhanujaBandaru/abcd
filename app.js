f10692bf670e2bbae995021df77a39612b31246c
dhanuja <dhanujabandaru2002@gmail.com>
2023-02-23 06:04:09 +0000

    commit message

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const convertDbObjectToResponseObject = (dbObject) => {
   };
 };
 app.get("/players/", async (request, response) => {
-  const sql = `SELECT * FROM cricket_team ORDER BY player_id;`;
+  const sql = `SELECT * FROM cricket_team;`;
   const cricketTeam = await db.all(sql);
   response.send(
     cricketTeam.map((eachPlayer) => convertDbObjectToResponseObject(eachPlayer))
@@ -45,14 +45,14 @@ app.get("/players/", async (request, response) => {
 app.post("/players/", async (request, response) => {
   const cricketDetails = request.body;
   const { playerName, jerseyNumber, role } = cricketDetails;
-  const sqls = `INSERT INTO cricket (playerName, jerseyNumber, role) VALUES (${playerName},${jerseyNumber},${role});`;
+  const sqls = `INSERT INTO cricket (playerName, jerseyNumber, role) VALUES ('${playerName}',${jerseyNumber},'${role}');`;
   const dbResponse = await db.run(sqls);
   const player_id = dbResponse.lastID;
-  response.send({ player_id: player_id });
+  response.send("Player Added to Team");
 });
 //API 3
 app.get("/players/:playerId/", async (request, response) => {
-  const { playerId } = request.params;
+  const { playerID } = request.params;
   const getcricket = `SELECT * FROM cricket_team WHERE player_id=${playerID};`;
   const cricket = await db.get(getcricket);
   response.send(cricket);
@@ -62,7 +62,7 @@ app.put("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
   const playerDetails = request.body;
   const { playerName, jerseyNumber, role } = playerDetails;
-  const updatePlayerQuery = `UPDATE cricket_team SET playerName=${playerName},jerseyNumber=${jerseyNumber},role=${role} WHERE player_id=${playerId};`;
+  const updatePlayerQuery = `UPDATE cricket_team SET player_name='${playerName}',jersey_number=${jerseyNumber},role='${role}' WHERE player_id=${playerId};`;
   await db.run(updatePlayerQuery);
   response.send("Player Details Updated");
 });
